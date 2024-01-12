89d2d43f155398a4ba080d98d9ebe01b609f507b
Loading <ashish.m@netwebindia.com>
2024-01-12 10:19:55 +0530

    made function to communicate with mongodb

--- backend-application/dbs/mongo/get_db.py
+++ backend-application/dbs/mongo/get_db.py
@@ -1,14 +1,21 @@
 from .mongo import PymongoDB
+from fastapi import HTTPException
+from fastapi import status
 
-def get_db():
+def get_db(db_name: str = None) -> any : 
+    """
+    This function help to connect database which provided in parameter
+    - Parameters : 
+        - db_name : select this database while querying in mongodb
+    """
     try:
-        db = PymongoDB().get_pymongo_client()['db_name']
+        db = PymongoDB().get_pymongo_client()[db_name]
         dbs = PymongoDB().get_pymongo_client().list_database_names()
-        if 'db_name' in dbs:
+        if db_name in dbs:
             return db
-        else:
-            return None
     except Exception as e:
-        return None
-    
-
+        pass
+    raise HTTPException(
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        detail="Database Connection Error"
+        )
