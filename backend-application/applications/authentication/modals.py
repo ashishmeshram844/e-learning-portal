63ff827050833da1f180963088e1521b52981551
Ashish <ashish.m@netwebindia.com>
2024-01-25 13:05:46 +0530

    authentication application docstrings addedd in all apis and functions

--- backend-application/applications/authentication/modals.py
+++ backend-application/applications/authentication/modals.py
@@ -1,12 +1,23 @@
+"""
+This module contain all modal schemas which are required
+in token operations
+"""
+
 from pydantic import BaseModel
 
 
 class TokenInput(BaseModel):
+    """"
+    modal uses for genrating token request data
+    """
     username : str
     password : str
 
 
 class TokenResponse(BaseModel):
+    """
+    Sample response modal for token response 
+    """
     access_token :str
     refresh_token : str
 
--- backend-application/applications/authentication/tables.py
+++ backend-application/applications/authentication/tables.py
@@ -1,3 +1,7 @@
+"""
+contain all tables name in database which we need in authentication application
+"""
+
 AUTH_TABLES = {
     'users' : 'users'
 }
